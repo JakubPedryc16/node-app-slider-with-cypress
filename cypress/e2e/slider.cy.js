5c28a79b6838c5b4cb152bc66a0bdbe6a1eb466b
JakubPedryc16 <jakubpedryc@gmail.com>
2025-04-01 18:25:30 +0200

    Update

--- cypress/e2e/slider.cy.js
+++ cypress/e2e/slider.cy.js
@@ -16,3 +16,27 @@ describe('Swiper Gallery Test', function () {
     cy.get('.swiper-slide-active').should('contain', 'Paris');
   });
 });
+
+describe('Galeria - Test responsywności Swiper', () => {
+  const devices = [
+    { name: 'desktop', width: 1280, height: 800 },
+    { name: 'tablet', width: 768, height: 1024 },
+    { name: 'mobile', width: 375, height: 812 }
+  ];
+
+  devices.forEach((device) => {
+    it('Sprawdzenie galerii na ${device.name', () => {
+      cy.viewport(device.width, device.height);
+      cy.visit('http://localhost:3000');
+
+      cy.get('.swiper').should('be.visible');
+
+      cy.get('.swiper-slide img').should('be.visible');
+
+      cy.get('.swiper-button-prev').should('be.visible').click();
+      cy.get('.swiper-button-next').should('be.visible').click();
+
+      cy.get('.swiper-pagination').should('be.visible');
+    });
+  });
+});
